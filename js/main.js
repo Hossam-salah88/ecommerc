7a7a2a21610d888dbc7f7a057fa6b2bca93946f4
Hossam-salah88 <eng.hegazy88@gmail.com>
2022-05-08 22:04:57 +0200

    special section

--- js/main.js
+++ js/main.js
@@ -1,13 +1,28 @@
 const navList = document.querySelector(".nav__list");
 const navToggelBtn = document.querySelector(".toggel__btn");
-const navbar = document.querySelector(".nav__logo");
+const navLogo = document.querySelector(".nav__logo");
+const navbar = document.querySelector(".nav");
+
+//========== toggel butten =================
 
 navToggelBtn.addEventListener("click", () => {
   navList.classList.toggle("active");
-  navbar.classList.toggle("open");
+  navLogo.classList.toggle("open");
 });
 
-// swiper js
+//=================== shadow on scroll ===================
+function scrollHeader() {
+  const scrollY = window.pageYOffset;
+  if (this.scrollY >= 80) {
+    navbar.classList.add("nav__scroll");
+  } else {
+    navbar.classList.remove("nav__scroll");
+  }
+}
+window.addEventListener("scroll", scrollHeader);
+
+//==================== swiper js ====================
+//================ hero Section =============
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: 1,
   loop: true,
@@ -15,5 +30,37 @@ var swiper = new Swiper(".mySwiper", {
     // nextEl: ".hossam",
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
+  },
+});
+
+// ================ Special Section ================
+var swiper = new Swiper(".special__mySwiper", {
+  slidesPerView: 4,
+  spaceBetween: 30,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 0,
+    },
+    576: {
+      slidesPerView: 1,
+      spaceBetween: 0,
+    },
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+  },
+  pagination: {
+    el: ".special__swiper-pagination",
+    clickable: true,
   },
 });
